deb472f73e607e62e6371420016e1e98086e2dfe
justinjest <jest.cayaban@gmail.com>
2024-08-23 16:24:05 +0900

    Added simple little triangle character

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-if __name__ == "__main__":
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    p = player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pass
-        screen.fill(000)
+                return 
+        screen.fill("black")
+        p.draw(screen) 
         pygame.display.flip()
+        dt = game_clock.tick(60)/1000
+
+
+if __name__ == "__main__":
     main()
