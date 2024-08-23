b2c5a2c171992be1c84382958255295c06ee54ef
justinjest <jest.cayaban@gmail.com>
2024-08-23 13:31:12 +0900

    Added basic screen drawing

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pass
+        screen.fill(000)
+        pygame.display.flip()
+    main()
