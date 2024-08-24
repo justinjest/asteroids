14172ece4585bf735ce7d1c6de9d23181840dd62
justinjest <jest.cayaban@gmail.com>
2024-08-24 21:39:44 +0900

    Adding asteroids, currently they all spawn in the upper left hand corner for some reason, unclear why

--- asteroid.py
+++ asteroid.py
@@ -1,12 +1,13 @@
-from circleshape import *
+import pygame
+import random
+from constants import *
+from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        super().__init__(x,y,radius)
-        self.position = pygame.Vector2 (x, y)
-        self.radius = radius
+        super().__init__(x,y,radius) 
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.radius, 2)
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
     def update(self, dt):
         self.position = self.velocity * dt
     
