7d3b31e7c89eb52a68ebd98477fbada4db729e50
justinjest <jest.cayaban@gmail.com>
2024-08-24 21:41:58 +0900

    Missing + sign in Asteroid.update was causing the problem, has been fixed.

--- asteroid.py
+++ asteroid.py
@@ -9,5 +9,5 @@ class Asteroid(CircleShape):
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
     def update(self, dt):
-        self.position = self.velocity * dt
+        self.position += self.velocity * dt
     
